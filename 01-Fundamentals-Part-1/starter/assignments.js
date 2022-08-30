b5144a1741926edc86903cbb8f4c2a41d3ab50ed
rfrf198231 <rfrf198231@gmail.com>
2022-08-30 16:11:14 +0200

    step_1

---
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -0,0 +1,44 @@
+const country = 'Ukraine'
+const continent = 'Europe'
+let population = 45000000
+const isIsland = 'Crimea'
+let language  = 'ukrainian'
+
+let description = `${country} is in ${continent}, and its ${population} people speak ${language}`;
+let descriptionTwo = country + ' is in '+ continent+ ', and its ' + population + ' people speak ' + language;
+console.log(description);
+console.log(descriptionTwo);
+
+console.log(country, continent, population, 
+isIsland, language, );
+
+
+let part = population / 2
+console.log(part);
+
+population++
+console.log(population);
+
+let finland = 6000000
+console.log(population > finland);
+
+let another = 33000000
+console.log(another > part);
+
+
+
+
+
+
+
+
+
+
+// const firstCountry = 'Portugal'
+// const firstContinent = 'Europe'
+// let firstPopulation = '10 000 000'
+// const isTwoIsland = 'Madeira'
+// let isLanguage = 'portuges'
+// console.log(firstCountry, firstContinent, firstPopulation,isTwoIsland, isLanguage );
+
+
---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,34 @@
+
+// const now = 3;
+// const ageS = 2022 - 2000;
+// const ageV = 2022 - 2001;
+// console.log(ageS + ageV);
+
+// console.log( 2 ** 3);
+// //  2  * 2 * 2
+
+// let x = 2 + 2;
+// x += 10;
+// x *= 2;
+// console.log(x);
+// let y = 5
+// y++ 
+// console.log(y);
+
+//17
+// const firstName = 'Serhii';
+// const job = 'police';
+// const birthYear = 1999;
+// const year = 2022;
+
+// const serhii = `I'm  ${firstName}, a ${year - birthYear} year old, now I working ${job} !`
+// console.log(serhii);
+
+// const sergo = "I'm " + firstName + ', a ' + (year - birthYear) + ' years old, ' + 'now i working ' + job+ ' !';
+// console.log(sergo);
+
+// console.log(`Just a regular string`);
+// console.log('Just \n\ a regular \n\ string');
+// console.log(`just
+// a reguar
+// string`);
