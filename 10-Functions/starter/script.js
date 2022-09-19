93d1f17ad3277a5f8b498107d620b3947c66d657
rfrf198231 <rfrf198231@gmail.com>
2022-09-19 22:02:52 +0200

    funk_step1

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,1 +1,26 @@
 'use strict';
+
+// Functions Accepting Callback Functions
+const oneWord = function (str) {
+  return str.replace(/ /g, '_').toLowerCase();
+};
+
+const upperFirstWord = function (str) {
+  const [first, ...others] = str.split(' ');
+  return [first.toUpperCase(), ...others].join('-');
+};
+
+// Higher-order function
+const transformer = function (str, fn) {
+  console.log(`Original string: ${str}`);
+  console.log(`Transformed string: ${fn(str)}`);
+  console.log(`Transformed by: ${fn.name}`);
+};
+
+transformer('JavaScript is the best!', upperFirstWord);
+transformer('JavaScript is the best!', oneWord);
+
+const high5 = function () {
+  console.log(`odf`);
+};
+document.body.addEventListener(`click`, high5);
