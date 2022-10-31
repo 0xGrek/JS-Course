98a8b87a454475cd3ea112190c0e42f1726c6972
Serhii <rfrf198231@gmail.com>
2022-10-31 21:22:59 +0000

    prom_2

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -112,9 +112,125 @@ setTimeout(() => {
 */
 
 // 251. Promises
-// const request = new XMLHttpRequest();
-//   request.open(`GET`, `https://restcountries.com/v3.1/name/${country}`);
-//   request.send();
+// const renderCountry = function (data, className = ``) {
+//   const html = `
+//     <article class="country ${className}">
+//       <img class="country__img" src="${data.flags.png}" />
+//       <div class="country__data">
+//         <h3 class="country__name">${data.name.common}</h3>
+//         <h4 class="country__region">${data.region}</h4>
+//         <p class="country__row"><span>👫</span>${(
+//           +data.population / 1000000
+//         ).toFixed(1)} people</p>
+//         <p class="country__row"><span>🗣️</span>${
+//           data.languages[Object.keys(data.languages)[0]]
+//         }</p>
+//         <p class="country__row"><span>💰</span>${
+//           data.currencies[Object.keys(data.currencies)[0]].name
+//         }</p>
+//       </div>
+//     </article>
+//     `;
+//   countriesContainer.insertAdjacentHTML('beforeend', html);
+//   countriesContainer.style.opacity = 1;
+// };
+// v.1.0
+// const getCountryData = function (country) {
+//   fetch(`https://restcountries.com/v3.1/name/${country}`)
+//     .then(function (response) {
+//       console.log(response);
+//       return response.json();
+//     })
+//     .then(function (data) {
+//       console.log(data);
+//       renderCountry(data[0]);
+//     });
+// };
 
-const request = fetch(`https://restcountries.com/v3.1/name/portugal`);
-console.log(request);
+// const request = fetch(`https://restcountries.com/v3.1/name/portugal`);
+/*
+const getCountryData = function (country) {
+  // Country 1
+  fetch(`https://restcountries.com/v3.1/name/${country}`)
+    .then(response => response.json())
+    .then(data => {
+      renderCountry(data[0]);
+      console.log(data);
+      const neighbour = data[0].borders[0];
+      console.log(neighbour);
+
+      if (!neighbour) return;
+
+      // Country 2
+      return fetch(`https://restcountries.com/v3.1/alpha/${neighbour}`);
+    })
+    .then(response => response.json())
+    .then(data => renderCountry(data[0], 'neighbour'));
+};
+
+// getCountryData(`portugal`);
+getCountryData(`usa`);
+*/
+// 254. Handling Rejected promise
+
+const renderCountry = function (data, className = ``) {
+  const html = `
+    <article class="country ${className}">
+      <img class="country__img" src="${data.flags.png}" />
+      <div class="country__data">
+        <h3 class="country__name">${data.name.common}</h3>
+        <h4 class="country__region">${data.region}</h4>
+        <p class="country__row"><span>👫</span>${(
+          +data.population / 1000000
+        ).toFixed(1)} people</p>
+        <p class="country__row"><span>🗣️</span>${
+          data.languages[Object.keys(data.languages)[0]]
+        }</p>
+        <p class="country__row"><span>💰</span>${
+          data.currencies[Object.keys(data.currencies)[0]].name
+        }</p>
+      </div>
+    </article>
+    `;
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  // countriesContainer.style.opacity = 1;
+};
+
+const renderError = function (msg) {
+  countriesContainer.insertAdjacentText(`beforeend`, msg);
+  // countriesContainer.style.opacity = 1;
+};
+
+const getCountryData = function (country) {
+  // Country 1
+  fetch(`https://restcountries.com/v3.1/name/${country}`)
+    .then(response => response.json())
+    .then(data => {
+      renderCountry(data[0]);
+      console.log(data);
+      const neighbour = data[0].borders[0];
+      console.log(neighbour);
+
+      if (!neighbour) return;
+
+      // Country 2
+      return fetch(`https://restcountries.com/v3.1/alpha/${neighbour}`);
+    })
+    .then(response => response.json())
+    .then(data => renderCountry(data[0], 'neighbour'))
+    // if promise rejected
+    .catch(err => {
+      console.error(`${err}`);
+      renderError(`Something went wrong 💥💥💥 ${err.message}.Try again!`);
+    })
+    // always use in finali promise
+    .finally(() => {
+      countriesContainer.style.opacity = 1;
+    });
+};
+
+btn.addEventListener(`click`, function () {
+  getCountryData(`SPAIN`);
+});
+
+// getCountryData(`portugal`);
