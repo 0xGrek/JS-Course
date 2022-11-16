c9fb031ea4e48d02dbad40c1acae1c76ad141ab5
Serhii <rfrf198231@gmail.com>
2022-11-16 17:32:02 +0000

    259.ss

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -173,75 +173,75 @@ getCountryData(`usa`);
 */
 // 254. Handling Rejected promise
 
-const renderCountry = function (data, className = ``) {
-  const html = `
-    <article class="country ${className}">
-      <img class="country__img" src="${data.flags.png}" />
-      <div class="country__data">
-        <h3 class="country__name">${data.name.common}</h3>
-        <h4 class="country__region">${data.region}</h4>
-        <p class="country__row"><span>👫</span>${(
-          +data.population / 1000000
-        ).toFixed(1)} people</p>
-        <p class="country__row"><span>🗣️</span>${
-          data.languages[Object.keys(data.languages)[0]]
-        }</p>
-        <p class="country__row"><span>💰</span>${
-          data.currencies[Object.keys(data.currencies)[0]].name
-        }</p>
-      </div>
-    </article>
-    `;
-  countriesContainer.insertAdjacentHTML('beforeend', html);
-  // countriesContainer.style.opacity = 1;
-};
+// const renderCountry = function (data, className = ``) {
+//   const html = `
+//     <article class="country ${className}">
+//       <img class="country__img" src="${data.flags.png}" />
+//       <div class="country__data">
+//         <h3 class="country__name">${data.name.common}</h3>
+//         <h4 class="country__region">${data.region}</h4>
+//         <p class="country__row"><span>👫</span>${(
+//           +data.population / 1000000
+//         ).toFixed(1)} people</p>
+//         <p class="country__row"><span>🗣️</span>${
+//           data.languages[Object.keys(data.languages)[0]]
+//         }</p>
+//         <p class="country__row"><span>💰</span>${
+//           data.currencies[Object.keys(data.currencies)[0]].name
+//         }</p>
+//       </div>
+//     </article>
+//     `;
+//   countriesContainer.insertAdjacentHTML('beforeend', html);
+//   countriesContainer.style.opacity = 1;
+// };
 
-const renderError = function (msg) {
-  countriesContainer.insertAdjacentText(`beforeend`, msg);
-  // countriesContainer.style.opacity = 1;
-};
+// const renderError = function (msg) {
+//   countriesContainer.insertAdjacentText(`beforeend`, msg);
+//   // countriesContainer.style.opacity = 1;
+// };
 
-const getJSON = function (url, errorMsg = 'Something went wrong') {
-  return fetch(url).then(response => {
-    if (!response.ok) throw new Error(`${errorMsg} (${response.status})`);
+// const getJSON = function (url, errorMsg = 'Something went wrong') {
+//   return fetch(url).then(response => {
+//     if (!response.ok) throw new Error(`${errorMsg} (${response.status})`);
 
-    return response.json();
-  });
-};
+//     return response.json();
+//   });
+// };
 
-const getCountryData = function (country) {
-  // Country 1
-  getJSON(
-    `https://restcountries.com/v3.1/alpha/${country}`,
-    `Country not found`
-  )
-    .then(data => {
-      renderCountry(data[0]);
+// const getCountryData = function (country) {
+//   // Country 1
+//   getJSON(
+//     `https://restcountries.com/v3.1/alpha/${country}`,
+//     `Country not found`
+//   )
+//     .then(data => {
+//       renderCountry(data[0]);
 
-      const neighbour = data[0].borders[0];
-      console.log(neighbour);
+//       const neighbour = data[0].borders[0];
+//       console.log(neighbour);
 
-      if (!neighbour) throw new Error(`No neighbour found`);
+//       if (!neighbour) throw new Error(`No neighbour found`);
 
-      // Country 2
-      getJSON(
-        `https://restcountries.com/v3.1/alpha/${neighbour}`,
-        `Neighbour not found`
-      );
-    })
-    .then(data => renderCountry(data[0], 'neighbour'))
-    .catch(err => {
-      console.error(`${err} 💥💥💥`);
-      renderError(`Something went wrong 💥💥 ${err.message}. Try again!`);
-    })
-    .finally(() => {
-      countriesContainer.style.opacity = 1;
-    });
-};
+//       // Country 2
+//       getJSON(
+//         `https://restcountries.com/v3.1/alpha/${neighbour}`,
+//         `Neighbour not found`
+//       );
+//     })
+//     .then(data => renderCountry(data[0], 'neighbour'))
+//     .catch(err => {
+//       console.error(`${err} 💥💥💥`);
+//       renderError(`Something went wrong 💥💥 ${err.message}. Try again!`);
+//     })
+//     .finally(() => {
+//       countriesContainer.style.opacity = 1;
+//     });
+// };
 
-btn.addEventListener(`click`, function () {
-  getCountryData(`usa`);
-});
+// btn.addEventListener(`click`, function () {
+//   getCountryData(`usa`);
+// });
 
 // getCountryData(`australia`);
 // OLD wersion
@@ -286,3 +286,42 @@ btn.addEventListener(`click`, function () {
 // btn.addEventListener(`click`, function () {
 //   getCountryData(`spain`);
 // });
+
+// #258
+// console.log(`Test start`);
+// setTimeout(() => console.log(`0 sec timer`), 0);
+// Promise.resolve(`Resolved promise 1`).then(res => console.log(res));
+
+// Promise.resolve(`Resolved promise 2`).then(res => {
+//   for (let i = 0; i < 10; i++) {}
+//   console.log(res);
+// });
+
+// console.log(`Test end`);
+
+//#259 Simple Promise
+const lotteryPromise = new Promise(function (reslove, reject) {
+  console.log(`Lotery draw is LOL`);
+  setTimeout(function () {
+    if (Math.random() >= 0.5) {
+      reslove(`You WIN 💦`);
+    } else {
+      reject(new Error(`You lost your money 💢`));
+    }
+  }, 200);
+});
+
+lotteryPromise.then(res => console.log(res)).catch(err => console.error(err));
+
+const wait = function (seconds) {
+  return new Promise(function (reslove) {
+    setTimeout(reslove, (seconds = 2000));
+  });
+};
+
+wait(2)
+  .then(() => {
+    console.log(`I waited for a two seconds`);
+    return wait(1);
+  })
+  .then(() => console.log(`I waited a one second`));
